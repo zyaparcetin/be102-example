1cb13ddfe92dc3dbfbfd43a4993b296f47c55f6b
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-11-16 16:52:31 +0000

    Fix style errors on index.js

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const armagan = new Person('armagan', 36)
 const berlinPhoto = new Photo('berlin.jpg')
 const munichPhoto = new Photo('munich.jpg')
 
-function foo() {
-  return 3 ? 6 : 8 ? 6 : 1
-}
 steve.addPhoto(berlinPhoto)
 steve.addPhoto(munichPhoto)
 
